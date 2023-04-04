a1ffc46cdcd74019b11df5b0c739194d42d74f52
Stéphane Gobancé <stgobance@airfrance.fr>
2023-04-04 17:20:39 +0200

    Update script

--- iac/terraform/terraform_check.sh
+++ iac/terraform/terraform_check.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
 terraform fmt -check iac/terraform
-if [ $? -ne 0 ]; then
+status=$?
+echo Terraform status: $status
+if [ $status -ne 0 ]; then
     echo "Terraform files are not properly formatted!"
     exit 1
 fi
